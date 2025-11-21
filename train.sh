91f9da231d8075f437c91203eae2d6fa0c7328eb
Fedorov Artem <artem.defender2016@mail.ru>
2025-11-21 12:53:42 +0300

    change in parameters of train

--- train.sh
+++ train.sh
@@ -5,12 +5,12 @@ accelerate launch --config_file /mnt/virtual_ai0001071-01239_SR006-nfs2/afedorov
     train.py \
     model=fsq_vqvae \
     trainer.num_steps=100000 \
+    dataset.batch_size=32 \
+    model.latent_dim=512 \
     dataset.batch_size=256 \
-    model.latent_dim=256 \
-    dataset.batch_size=256 \
-    trainer.log_interval=100 \
-    trainer.val_interval=3000 \
-    trainer.save_interval=25000 \
+    trainer.log_interval=5 \
+    trainer.val_interval=200 \
+    trainer.save_interval=1000 \
     trainer.gradient_accumulation_steps=1 \
     trainer.mixed_precision="bf16" \
     experiment_name=fsq_vqvae_initial
